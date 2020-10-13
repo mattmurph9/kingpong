978fad2845a9365ab8bba1ae19e4b403a0090d4a
Matt Murphy <matt@Matts-MacBook-Pro.local>
2020-10-13 07:41:23 +0200

    Add some initial functionality
    This includes ability to register a player, which stores the player
    in MongoDB.

---
+++ db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const playerSchema = mongoose.Schema({
+	name: { type: String, unique: true },
+	wins: Number,
+	losses: Number,
+	elo: Number,
+});
+
+// Connect to mongodb
+module.exports = {
+  connectToDb: () => {
+    const mongoPass = process.env.MONGOPASS;
+    const mongoUser = process.env.MONGOUSER;
+    const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cluster0.a73d5.mongodb.net/kingpong?retryWrites=true&w=majority`;
+    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+    const db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'connection error:'));
+
+    db.once('open', function() {
+      console.log("Successfully connected to MongoDB!");
+    });
+
+    return db;
+  },
+  Player: mongoose.model('Player', playerSchema)
+}
+
+
--- index.js
+++ index.js
@@ -1,74 +1,68 @@
-const { createEventAdapter } = require('@slack/events-api');
-const { WebClient } = require('@slack/web-api');
+const { App } = require('@slack/bolt');
+const { connectToDb, Player } = require('./db.js');
 
-const token = process.env.OAUTH_TOKEN;
-const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+// Connect to slack with bolt
+const app = new App({
+	token: process.env.SLACK_BOT_TOKEN,
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
 
-const slackEvents = createEventAdapter(slackSigningSecret);
-const web = new WebClient(token);
-const port = process.env.PORT || 3000;
+const db = connectToDb();
 
-/**
- * @type <Record<string, number>> A record of the word and score. Should start at 0.
- * This should be replaced by a database for persistence. This is just a demo and as
- * such simply mutates this object to be stateful.
- */
-const state = {};
-
-/**
- * A function that accepts a string, then returns the action and the word to score.
- */
-const getIsPlusOrMinus = str => {
-	console.log('in getPlusOrMinus');
-	// Accept em-dash for cases like MacOS turning -- into an emdash
-	const plusOrMinusRegex = /\@(\w+?)(\-{2}|\+{2}|\—{1})/;
-  // The first item in the array is the full string, then the word to score, then the operator
-	const [_, itemToScore, scoreStr] = plusOrMinusRegex.exec(str) || [];
-	switch (scoreStr) {
-		case '--':
-		case '—':
-			return { action: 'minus', word: itemToScore };
-		case '++':
-			return { action: 'add', word: itemToScore };
-		default:
-			return { action: '', word: undefined };
-	}
-};
-
-slackEvents.on('message', async event => {
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
-  
-	const { action, word } = getIsPlusOrMinus(event.text);
-	console.log('action: ', action, 'word: ', word);
-  // If the `event.text` did not include a score (of plus or minus), it will return `{}`
-  // And therefore `action` will be `undefined`
-	if (action) {
-		const currentState = state[word] || 0;
-    // Mutate the state to update the score of the word.
-		state[word] = action == 'add' ? currentState + 1 : currentState - 1;
-		const actionString = action == 'add' ? 'had a point added' : 'had a point removed';
-		const result = await web.chat.postMessage({
-			text: `${word} ${actionString}. Score is now at: ${state[word]}`,
+app.event('app_mention', async ({ event, client }) => {
+	try {
+		const invalidMessage = async () => await client.chat.postMessage({
+			text: 'Invalid format',
 			channel: event.channel,
 		});
 
-		console.log(`Successfully send message ${result.ts} in conversation ${event.channel}`);
-	}
-
-  // Check if the text includes the text we'd want to use to check the leaderboard
-	if (/@kingpongs leaderboard/i.exec(event.text)) {
-		const result = await web.chat.postMessage({
-      // We'll add more functionality in the future. We just want to test it works, first
-			text: 'This should output a leaderboard',
-			channel: event.channel,
-		});
-
-		console.log(`Successfully send message ${result.ts} in conversation ${event.channel}`);
+		console.log(`Received a mention: user ${event.user} in channel ${event.channel} says ${event.text}`);
+		const words = event.text.split(' ');
+		console.log(words);
+		if(words.length < 2) {
+			invalidMessage();
+		}
+		const action = words[1];
+		switch(action) {
+			case 'register':
+				console.log('register');
+				await Player.create({ name: words[2], wins: 0, losses: 0, elo: 1000 });
+				await client.chat.postMessage({
+					text: 'Unrecognized action. Try `register` or `match`.',
+					channel: event.channel,
+				});
+				break;
+			case 'match':
+				console.log('match');
+				const player1 = words[2];
+				const player2 = words[3];
+				client.chat.postMessage({
+					text: words[2],
+					channel: event.channel,
+				});
+				break;
+		  default:
+				await client.chat.postMessage({
+					text: 'Unrecognized action. Try `register` or `match`.',
+					channel: event.channel,
+				});
+		}
+	} catch (e) {
+		if ( e.code && e.code === 11000) {
+			await client.chat.postMessage({
+				text: 'User already exists!',
+				channel: event.channel,
+			});
+		} else {
+			await client.chat.postMessage({
+				text: 'There was an error. Please try again.',
+				channel: event.channel,
+			});
+		}
 	}
 });
 
-slackEvents.on('error', console.error);
-
-slackEvents.start(port).then(() => {
-	console.log(`server listening on port ${port}`);
-});
+(async () => {
+	await app.start(process.env.PORT || 3000);
+	console.log('Bolt app running');
+})();
